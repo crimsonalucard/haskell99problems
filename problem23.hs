c7f9fae03dc8e1107b256afbd91fd4812aed1585
brian <heynairb@gmail.com>
2015-07-07 08:23:41 -0700

    started problem 23

--- problem23.hs
+++ problem23.hs
@@ -9,8 +9,8 @@ recurRandom :: [a] -> Int -> StdGen -> [a]
 recurRandom _  0 _   = []
 recurRandom xs n gen = let randomPair = randomR (1, length xs) gen
                            randomNum = fst randomPair
-                           gen = snd randomPair
-                           in randomNum:(recurRandom (delete randomNum xs) n-1 gen) 
+                           newgen = snd randomPair
+                           in randomNum:(recurRandom (delete randomNum xs) n-1 newgen) 
 
 rnd_select :: [a] -> Int -> [a]
 rnd_select xs n = do 
