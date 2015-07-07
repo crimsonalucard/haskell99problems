cb7c1c6b2b4c164e9626111847170e451425ed63
brian <heynairb@gmail.com>
2015-07-07 08:33:59 -0700

    started problem 23

--- problem23.hs
+++ problem23.hs
@@ -1,11 +1,11 @@
 #!/usr/bin/env runghc
-
+--NOT COMPLETE
 import System.Random
 import Data.List
 
 
 
-recurRandom :: [a] -> Int -> StdGen -> [a]
+recurRandom :: (Eq a) => [a] -> Int -> StdGen -> [a]
 recurRandom _  0 _   = []
 recurRandom xs n gen = let randomPair = randomR (1, length xs) gen
                            randomIndex = fst randomPair
