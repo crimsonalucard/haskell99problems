9dbf7940e068c90cb3e2b4afd6f771d23664e4f4
brian <heynairb@gmail.com>
2015-07-08 09:05:33 -0700

    started problem 23

--- problem23.hs
+++ problem23.hs
@@ -1,5 +1,5 @@
 #!/usr/bin/env runghc
---NOT COMPLETE
+--NOT COMPLETE INDEX IS SOMETIMES TOO LARGE
 import System.Random
 import Data.List
 
@@ -9,12 +9,11 @@ recurRandom :: (Eq a) => [a] -> Int -> StdGen -> [a]
 recurRandom _  0 _   = []
 recurRandom xs n gen = let randomPair = randomR (1, length xs) gen
                            randomIndex = fst randomPair
-                           randomNum = xs !! randomIndex
+                           randomNum = xs !! (randomIndex - 1)
                            newgen = snd randomPair
-                           in randomNum:(recurRandom (delete randomNum xs) (n-1) newgen) 
-
+                --           in randomNum:(recurRandom (delete randomNum xs) (n-1) newgen) 
+                           in randomNum:(recurRandom xs (n-1) newgen)
 rnd_select :: Eq a => [a] -> Int -> IO [a]
 rnd_select xs n = do 
                     gen <- newStdGen
                     return $ recurRandom xs n gen
-
